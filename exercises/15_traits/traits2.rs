e1ad34e5bb2bb4a7866e195b7189b2dcf1b7be76
donavank <donavankeen97@gmail.com>
2023-12-31 16:48:53 -0500

    Complete up to quiz3

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -8,13 +8,17 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut vec_copy = self.clone();
+        vec_copy.push(String::from("Bar"));
+        return vec_copy;
+    }
+}
 
 #[cfg(test)]
 mod tests {
